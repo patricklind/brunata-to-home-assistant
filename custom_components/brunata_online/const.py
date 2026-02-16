36eebd9c9c99e5bf8e8363edd434eedcc2a01ccf
Patrick Lind <19758127+patricklind@users.noreply.github.com>
2026-02-16 13:10:13 +0100

    Bump version to 0.9.3

--- custom_components/brunata_online/const.py
+++ custom_components/brunata_online/const.py
@@ -4,7 +4,7 @@
 NAME = "Brunata Online"
 DOMAIN = "brunata_online"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.9.2"
+VERSION = "0.9.3"
 
 ATTRIBUTION = "Data provided by online.brunata.com"
 ISSUE_URL = "https://github.com/patricklind/brunata-to-home-assistant/issues"
